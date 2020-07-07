f2ce333280facd7522ac337797426a07e5721507
MARY ASCH <mary.mahoney13@gmail.com>
2020-07-07 10:52:28 -0400

    flushes set

--- hand_calculator.py
+++ hand_calculator.py
@@ -1,24 +1,41 @@
 from card import Card
 
 class HandCalculator():
-    def calculateScore(self, hand, cutCard):
+    def calculateScore(self, hand, cutCard, isKitty):
         score=0
 
         rightsuit = cutCard.suit
         if Card(rightsuit, "J") in hand:
             score+=1
 
-        isHandFlush = True
-        for card in hand:
-            if hand[0].suit == card.suit:
-                pass
-            else: 
-                isHandFlush = False 
-                break
-        if isHandFlush == True:
-            score += 4
-            if cutCard.suit == hand[0].suit:
-                score +=1
+        if isKitty == False:
+            isHandFlush = True
+            for card in hand:
+                if hand[0].suit == card.suit:
+                    pass
+                else: 
+                    isHandFlush = False 
+                    break
+            if isHandFlush == True:
+                score += 4
+                if cutCard.suit == hand[0].suit:
+                    score +=1
+        
+        if isKitty == True:
+            isHandFlush = True
+            if hand[0].suit != cutCard.suit:
+                isHandFlush = False
+            for card in hand:
+                if hand[0].suit == card.suit:
+                    pass
+                else: 
+                    isHandFlush = False
+                    break
+
+            if isHandFlush == True:
+                score += 5
+
+            
         
 
         return score
--- test_hand_calculator.py
+++ test_hand_calculator.py
@@ -6,33 +6,61 @@ from card import Card
 class TestHandCalculator(unittest.TestCase):
     def testRightJack(self):
         hc=HandCalculator()
-        score = hc.calculateScore([Card("Hearts", "4"), Card("Spade", "J"), Card("Club", "6"), Card("Club", "7")], Card("Spade", "3"))
+        score = hc.calculateScore([Card("Hearts", "4"), Card("Spade", "J"), Card("Club", "6"), Card("Club", "7")], Card("Spade", "3"), False)
         self.assertEqual(1,score)
     def testZeroHand(self):
         hc=HandCalculator()
-        score = hc.calculateScore([Card("Hearts", "4"), Card("Spade", "J"), Card("Club", "6"), Card("Club", "7")], Card("Heart", "3"))
+        score = hc.calculateScore([Card("Hearts", "4"), Card("Spade", "J"), Card("Club", "6"), Card("Club", "7")], Card("Heart", "3"), False)
         self.assertEqual(0,score)
     def testFlushHandWithCut(self):
         hc = HandCalculator()
         score = hc.calculateScore(
             [Card("Hearts", "2"), Card("Hearts", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
-            Card("Hearts", "10")
+            Card("Hearts", "10"), False
         )
         self.assertEqual(5,score)
     def testFlushHandWithOutCut(self):
         hc = HandCalculator()
         score = hc.calculateScore(
             [Card("Hearts", "2"), Card("Hearts", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
-            Card("Club", "10")
+            Card("Club", "10"), False
         )
         self.assertEqual(4,score)   
+
     def testFlushHandZero(self):
         hc = HandCalculator()
         score = hc.calculateScore(
-            [Card("Hearts", "2"), Card("Spade", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
-            Card("Hearts", "10")
+            [Card("Hearts", "2"), Card("Club", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Hearts", "10"), False
         )
         self.assertEqual(0,score)         
-            
+
+    def testFlushKittyWithCut(self):
+        hc = HandCalculator()
+        score = hc.calculateScore(
+            [Card("Hearts", "2"), Card("Hearts", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Hearts", "10"), True
+        )
+        self.assertEqual(5,score)
+
+    def testFlushKittyWithOutCut(self):
+        hc = HandCalculator()
+        score = hc.calculateScore(
+            [Card("Hearts", "2"), Card("Hearts", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Club", "10"), True
+        )
+        self.assertEqual(0,score)   
+    def testFlushKittyZero(self):
+        hc = HandCalculator()
+        score = hc.calculateScore(
+            [Card("Hearts", "2"), Card("Club", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Hearts", "10"), True
+        )
+        self.assertEqual(0,score)   
+
+
+
+
+
 if __name__ == "__main__":
     unittest.main() 
