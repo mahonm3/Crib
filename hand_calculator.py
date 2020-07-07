f4877f0ab09f6f077c55571de4de0343ff1a532a
MARY ASCH <mary.mahoney13@gmail.com>
2020-07-07 10:31:53 -0400

    flush in hand

--- hand_calculator.py
+++ hand_calculator.py
@@ -2,10 +2,25 @@ from card import Card
 
 class HandCalculator():
     def calculateScore(self, hand, cutCard):
-        rightsuit = cutCard.suit
         score=0
+
+        rightsuit = cutCard.suit
         if Card(rightsuit, "J") in hand:
             score+=1
+
+        isHandFlush = True
+        for card in hand:
+            if hand[0].suit == card.suit:
+                pass
+            else: 
+                isHandFlush = False 
+                break
+        if isHandFlush == True:
+            score += 4
+            if cutCard.suit == hand[0].suit:
+                score +=1
+        
+
         return score
 
 
--- test_hand_calculator.py
+++ test_hand_calculator.py
@@ -12,6 +12,27 @@ class TestHandCalculator(unittest.TestCase):
         hc=HandCalculator()
         score = hc.calculateScore([Card("Hearts", "4"), Card("Spade", "J"), Card("Club", "6"), Card("Club", "7")], Card("Heart", "3"))
         self.assertEqual(0,score)
-        
+    def testFlushHandWithCut(self):
+        hc = HandCalculator()
+        score = hc.calculateScore(
+            [Card("Hearts", "2"), Card("Hearts", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Hearts", "10")
+        )
+        self.assertEqual(5,score)
+    def testFlushHandWithOutCut(self):
+        hc = HandCalculator()
+        score = hc.calculateScore(
+            [Card("Hearts", "2"), Card("Hearts", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Club", "10")
+        )
+        self.assertEqual(4,score)   
+    def testFlushHandZero(self):
+        hc = HandCalculator()
+        score = hc.calculateScore(
+            [Card("Hearts", "2"), Card("Spade", "4"), Card("Hearts", "6"), Card("Hearts", "8")],
+            Card("Hearts", "10")
+        )
+        self.assertEqual(0,score)         
+            
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main() 
