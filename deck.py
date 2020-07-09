001a2c2ee808177b332b5a39567de4c49aab1414
MARY ASCH <mary.mahoney13@gmail.com>
2020-07-09 12:17:55 -0400

    add drawCard

--- deck.py
+++ deck.py
@@ -1,4 +1,5 @@
 from card import Card
+import random
 class Deck():
     def __init__(self):
         self.cards= []
@@ -6,4 +7,9 @@ class Deck():
             for face in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]:
                 c=Card(suit, face)
                 self.cards.append(c)
+        random.shuffle(self.cards)
+
+    def drawCard(self):
+        return self.cards.pop()
+        
     
